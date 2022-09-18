b966226ab6c197fd76900c4fa1bc50575cc43901
Adeniyi Moronfolu <saintniyi@yahoo.com>
2022-09-18 00:37:58 -0600

    Add DbInitializer class that will implement migrations to new database and also seed the database with data

---
+++ StudInfoDataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudInfoDataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudInfoDataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+
+        private readonly AppDbContext _db;
+
+
+        public DbInitializer(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Initialize()
+        {
+            //migrations if they are not applied
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+
+    }
+}
